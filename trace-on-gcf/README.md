4dafbf646be07cb97bc997d772081f3df46f6ac0
tanishiking24 <rikiriki1238@gmail.com>
2020-01-13 19:08:16 +0900

    Initial commit

---
+++ trace-on-gcf/README.md
@@ -0,0 +1,9 @@
+## Prequirement
+- [nodenv](https://github.com/nodenv/nodenv)
+- yarn
+
+### Created tsconfig from
+
+```sh
+$ npx tsc --init --rootDir src --outDir build --esModuleInterop --resolveJsonModule --lib es6 --module commonjs --allowJs true --noImplicitAny true
+```
---
+++ trace-on-gcf/src/index.ts
@@ -0,0 +1,110 @@
+import * as tracer from "@google-cloud/trace-agent";
+
+const projectId = process.env.GCP_PROJECT;
+let traceApi: tracer.PluginTypes.Tracer | undefined;
+if (process.env.NODE_ENV === "production") {
+  traceApi = tracer.start({
+    projectId,
+    logLevel: 4, // debug
+    bufferSize: 10
+  });
+}
+
+import { PubsubMessage } from "@google-cloud/pubsub/build/src/publisher";
+import { Context } from "@google-cloud/functions-framework";
+import { LoggingWinston } from "@google-cloud/logging-winston";
+
+import * as winston from "winston";
+import * as Transport from "winston-transport";
+
+function createLogger(): winston.Logger {
+  const loggingWinston = new LoggingWinston();
+  const transports: Transport[] = [
+    process.env.NODE_ENV === "production"
+      ? loggingWinston
+      : new winston.transports.Console()
+  ];
+  return winston.createLogger({
+    level: "info",
+    transports
+  });
+}
+
+const logger = createLogger();
+
+export async function traceOnGcf(
+  message: PubsubMessage,
+  context: Context
+): Promise<void> {
+  if (traceApi) {
+    traceApi.runInRootSpan({ name: "trace-on-gcf-root" }, async rootSpan => {
+      await run(message, context, rootSpan);
+    });
+  } else {
+    await run(message, context);
+  }
+  return;
+}
+
+async function run(
+  message: PubsubMessage,
+  context: Context,
+  rootSpan?: tracer.PluginTypes.RootSpan
+): Promise<void> {
+  const traceId = rootSpan?.getTraceContext()?.traceId;
+  const trace =
+    typeof traceId !== "undefined"
+      ? `projects/${projectId}/traces/${traceId}`
+      : undefined;
+
+  logger.info("start-function", { trace, context });
+
+  logger.warn("test-warn", { trace, text: "test" });
+
+  const sleep100 = await runWithChildSpan(
+    async () => {
+      return await sleep(100);
+    },
+    { name: "sleep100" },
+    rootSpan
+  );
+
+  const sleep150 = await runWithChildSpan(
+    async () => {
+      return await sleep(150);
+    },
+    { name: "sleep150" },
+    rootSpan
+  );
+
+  if (rootSpan) rootSpan.endSpan();
+}
+
+function runWithChildSpan<T>(
+  callback: () => T,
+  spanOptions: tracer.PluginTypes.SpanOptions,
+  rootSpan?: tracer.PluginTypes.RootSpan
+): T {
+  if (rootSpan) {
+    const childSpan = rootSpan.createChildSpan(spanOptions);
+    const res = callback();
+    childSpan.endSpan();
+    return res;
+  } else {
+    return callback();
+  }
+}
+
+/**
+ *
+ * @param duration sleep time (milliseconds)
+ * @param trace traceId that is passed to stackdriver logging.
+ */
+const sleep = (duration: number, trace?: string): Promise<number> => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      logger.info(`finished-sleep-${duration}`, { trace });
+      resolve(duration);
+    }, duration);
+  });
+};
